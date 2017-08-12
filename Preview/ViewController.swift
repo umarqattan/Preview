63a2bb537bf35d26e011a8bb0eb5303989164ec5
Umar Qattan <umar.qattan@mobileforming.com>
2017-08-12 16:19:59 -0700

    Initial Commit

---
+++ Preview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Preview
+//
+//  Created by Umar Qattan on 8/12/17.
+//  Copyright © 2017 Umar Qattan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
