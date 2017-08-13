9b2ff39a7184f1fc37913d273e298e893f4eae19
Umar Qattan <umar.qattan@mobileforming.com>
2017-08-12 17:05:04 -0700

    Update: New Project

---
+++ Preview/CameraViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CameraViewController.swift
+//  Preview
+//
+//  Created by Umar Qattan on 8/12/17.
+//  Copyright © 2017 Umar Qattan. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    var imagePicker:UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    @IBAction func takePhoto(_ sender: UIBarButtonItem) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func loadPhoto(_ sender: UIBarButtonItem) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: {self.imageLabel.isHidden = true})
+    }
+    
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+}
+
